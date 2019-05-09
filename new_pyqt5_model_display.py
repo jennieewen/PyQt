a1d39659085379ad27a53feffa2601f78ce51423
wxml <294036748@qq.com>
2019-05-09 21:31:37 +0800

    add pic while kuang

--- new_pyqt5_model_display.py
+++ new_pyqt5_model_display.py
@@ -13,8 +13,6 @@ from ReadHelper import ReadHelper
 from PyQt5.QtGui import QPainter, QColor, QBrush
 
 
-
-
 class Example(QWidget):
 
     def __init__(self):
@@ -24,22 +22,20 @@ class Example(QWidget):
         self.setFixedSize(1200, 800)
         # self.setWindowFlags(QtCore.Qt.FramelessWindowHint | QtCore.Qt.WindowStaysOnTopHint)
 
+        # 顶部 打开文件和地址栏
         self.button_open = QPushButton("打开FBX模型文件")
         self.button_open.setObjectName("open")
         self.path_edit = QLineEdit()
         self.path_edit.setObjectName("path")
-        # self.path_edit.setDisabled(True) # 无法选中和编辑
         self.button_exit = QPushButton("保存并退出")
         self.button_exit.setObjectName("exit")
 
         self.h_box1 = QHBoxLayout()
-        # self.h_box1.addStretch(1)
         self.h_box1.addWidget(self.button_open)
         self.h_box1.addStretch(1)
         self.h_box1.addWidget(self.path_edit, 50)
         self.h_box1.addStretch(1)
         self.h_box1.addWidget(self.button_exit)
-        # self.h_box1.addStretch(1)
 
         self.h_box2 = QHBoxLayout()
         self.g_tree = QtWidgets.QGridLayout()
@@ -47,12 +43,19 @@ class Example(QWidget):
 
         self.v_box = QVBoxLayout()
 
-        self.lg_pic = QtWidgets.QToolButton()
+        # 右边中间大图
+        self.lg_pic = QToolButton()
         self.lg_pic.setIcon(QtGui.QIcon("./bird.jpg"))  # 设置按钮图标
-        self.lg_pic.setIconSize(QtCore.QSize(900, 450))
-        self.v_box.addWidget(self.lg_pic)
-        self.h_box2.addLayout(self.v_box)
+        self.lg_pic.setIconSize(QtCore.QSize(873, 450))
 
+        self.g_lg_pic = QGridLayout()
+        self.g_lg_pic_widget = QWidget()
+        self.g_lg_pic_widget.setObjectName("lg_pic_layout")
+        self.g_lg_pic_widget.setLayout(self.g_lg_pic)
+
+        self.v_box.addWidget(self.g_lg_pic_widget)
+        self.g_lg_pic.addWidget(self.lg_pic)
+        self.h_box2.addLayout(self.v_box)
 
         self.h_bottom_box = QHBoxLayout()
         self.v_box.addLayout(self.h_bottom_box)
@@ -126,14 +129,12 @@ class Example(QWidget):
         with open('./style_bottom.qss', 'r') as f:
             self.v_bottom_box_left_widget.setStyleSheet(f.read())
 
+        # 主要布局设置
         self.h_box1_widget = QtWidgets.QWidget()
         self.h_box1_widget.setLayout(self.h_box1)
         self.main_box = QVBoxLayout()
-        # main_box.addStretch(1)
         self.main_box.addWidget(self.h_box1_widget)
-        # main_box.addStretch(1)
         self.main_box.addLayout(self.h_box2)
-        # main_box.addStretch(1)
         self.setLayout(self.main_box)
 
         self.tree_widget = QTreeWidget(self)
@@ -141,48 +142,12 @@ class Example(QWidget):
         self.tree_widget.header().setVisible(False)
         self.g_tree.addWidget(self.tree_widget, 0, 0, 1, 2)
 
-        # test tree style (need delete later)
-
-        str_path = "./a.txt"
-        rh = ReadHelper(str_path)
-        global global_rh
-        global_rh = rh
-        key_list = rh.key_list
-        # print(key_list)
-
-        self.root = QtWidgets.QTreeWidgetItem(self.tree_widget)  # QTreeWidgetItem object: self.root
-        # 取消自带的小箭头
-        self.tree_widget.setRootIsDecorated(False)
-        # 取消原来的选中蓝色效果
-        self.tree_widget.setSelectionMode(QAbstractItemView.NoSelection)
-        # 取消原来的选中框框
-        self.tree_widget.setFocusPolicy(QtCore.Qt.NoFocus)
-        # self.tree_widget.setFocusPolicy(False)  same effect as above line
-
-        self.root.setIcon(0, QtGui.QIcon("icons/closed_folder.png"))
-        self.root.setText(0, " " + rh.name + "                   ▾")  # set text of self.root
-        col = QColor(0, 124, 176)  # 0 124 176 blue
-        # col.setNamedColor("#fff")
-        self.root.setBackground(0, col)
-        self.root.setSizeHint(0, QtCore.QSize(45, 45))
-
-        self.child = []
-        for i in range(len(key_list)):
-            child_ = QtWidgets.QTreeWidgetItem(self.root)  # child of root
-            child_.setText(0, "    " + key_list[i])
-            child_.setFont(0, QtGui.QFont("Microsoft YaHei", 10))
-            child_.setSizeHint(0, QtCore.QSize(45, 45))
-            # print(child_)
-            self.child.append(child_)
-
-        # test tree style end
-
         # CONNECT FUNCTION PART
+
         self.button_open.pressed.connect(self.open_file_names_dialog)
         self.button_choose.pressed.connect(self.select_material)
         self.tree_widget.itemClicked['QTreeWidgetItem*', 'int'].connect(self.tree_item_click)
         # # 其中tree_item_click是自己定义的槽函数
-        self.tree_widget.itemSelectionChanged.connect(self.tree_item_change)
 
     '''open file function'''
     def open_file_names_dialog(self):
@@ -244,7 +209,6 @@ class Example(QWidget):
                 # print(child_)
                 self.child.append(child_)
 
-
     '''choose tree_item function'''
     def tree_item_click(self, item, n):
         # 把上次的颜色恢复成白色
@@ -262,7 +226,6 @@ class Example(QWidget):
                 self.tree_widget.expandItem(item)
                 self.root.setIcon(0, QtGui.QIcon("icons/open_folder.png"))
                 self.root.setText(0, " " + global_rh.name + "                   ▴")
-
         # 如果当前item是孩子节点
         else:
             # print(n)
@@ -291,10 +254,6 @@ class Example(QWidget):
             print(details)
             self.sub_model_info_info.setText(details)
 
-    '''select change function'''
-    def tree_item_change(self):
-        print("select change")
-
     '''select material function'''
     def select_material(self):
         options = QFileDialog.Options()
@@ -311,8 +270,6 @@ class Example(QWidget):
         self.lower_right_label.setText("      材质类型 : ")
 
         self.lower_right_label_detail.setText("         " + file_name)
-
-
 
     def on_button_click(self):
         q = QApplication.instance()
